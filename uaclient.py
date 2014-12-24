166247ef4e3dd83db3a31660afabd0d4cc4ee61a
Javier Martínez Molina <jmartinezm90@gmail.com>
2014-12-24 01:37:31 +0100

    optimizacion de codigo para guardar las etiquetas en un diccionario y comienzo en la introduccion de parametros por linea de comandos y en la utilizacion dichos parametros

--- uaclient.py
+++ uaclient.py
@@ -5,6 +5,9 @@
 
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
+import socket
+import sys
+import os
 
 
 class XMLHandler(ContentHandler):
@@ -15,7 +18,8 @@ class XMLHandler(ContentHandler):
             'rtpaudio': ['puerto'],
             'regproxy': ['ip', 'puerto'],
             'log': ['path'],
-            'audio': ['path']}   
+            'audio': ['path']}
+  
         self.list_etiquetas = []
         
         self.dic_etiq = {'account_username': '', 'account_passwd': '',
@@ -30,67 +34,86 @@ class XMLHandler(ContentHandler):
             for atributo in self.etiquetas[name]:
                 dic[atributo] = attrs.get(atributo, "")
             self.list_etiquetas.append(dic)
-
-
-    def get_tags(self):
-        return self.list_etiquetas
-
-    def __str__(self):
-        salida = ""
-        for dic in self.list_etiquetas:
-            salida += dic["name"] + "\t"
-            for etiqueta in dic:
-                if dic["name"] != dic[etiqueta] and dic[etiqueta] != "":
-                    salida += etiqueta + "=" + '"' + dic[etiqueta] + '"' + "\t"
-            salida += "\n"
-        return salida
-
-    def extraer_valores(self):
         for dic in self.list_etiquetas:
             for etiqueta in dic:
                 if dic["name"] == 'account':
-                        if etiqueta == "username":
-                            self.dic_etiq['account_username'] = dic[etiqueta]
-                            print self.dic_etiq['account_username']
-                        if etiqueta == "passwd":
-                            self.dic_etiq['account_passwd'] = dic[etiqueta]
-                            print self.dic_etiq['account_passwd']
+                    if etiqueta == "username":
+                        self.dic_etiq['account_username'] = dic[etiqueta]
+                    if etiqueta == "passwd":
+                        self.dic_etiq['account_passwd'] = dic[etiqueta]
                 if dic["name"] == 'uaserver':
-                        if etiqueta == "ip":
-                            self.dic_etiq['uaserver_ip'] = dic[etiqueta]
-                            print self.dic_etiq['uaserver_ip']
-                        if etiqueta == "puerto":
-                            self.dic_etiq['uaserver_puerto'] = dic[etiqueta]
-                            print self.dic_etiq['uaserver_puerto']
+                    if etiqueta == "ip":
+                        self.dic_etiq['uaserver_ip'] = dic[etiqueta]
+                    if etiqueta == "puerto":
+                        self.dic_etiq['uaserver_puerto'] = dic[etiqueta]
                 if dic["name"] == 'rtpaudio':
-                        if etiqueta == "puerto":
-                            self.dic_etiq['rtpaudio_puerto'] = dic[etiqueta]
-                            print self.dic_etiq['rtpaudio_puerto']
+                    if etiqueta == "puerto":
+                        self.dic_etiq['rtpaudio_puerto'] = dic[etiqueta]
                 if dic["name"] == 'regproxy':
-                        if etiqueta == "ip":
-                            self.dic_etiq['regproxy_ip'] = dic[etiqueta]
-                            print self.dic_etiq['regproxy_ip']
-                        if etiqueta == "puerto":
-                            self.dic_etiq['regproxy_puerto'] = dic[etiqueta]
-                            print self.dic_etiq['regproxy_puerto']
+                    if etiqueta == "ip":
+                        self.dic_etiq['regproxy_ip'] = dic[etiqueta]
+                    if etiqueta == "puerto":
+                        self.dic_etiq['regproxy_puerto'] = dic[etiqueta]
                 if dic["name"] == 'log':
-                        if etiqueta == "path":
-                            self.dic_etiq['log_path'] = dic[etiqueta]
-                            print self.dic_etiq['log_path']
+                    if etiqueta == "path":
+                        self.dic_etiq['log_path'] = dic[etiqueta]
                 if dic["name"] == 'audio':
-                        if etiqueta == "path":
-                            self.dic_etiq['audio_path'] = dic[etiqueta]
-                            print self.dic_etiq['audio_path']
-                   
-                        
+                    if etiqueta == "path":
+                        self.dic_etiq['audio_path'] = dic[etiqueta]
+
 
 if __name__ == "__main__":
 
+
+    list_metodo = ['INVITE', 'REGISTER', 'BYE']
+
+    #Comprobación de posibles excepciones
+    if len(sys.argv) != 4:
+        print 'Usage: python uaclient.py config method option'
+        raise SystemExit
+
+    if sys.argv[2] not in list_metodo:
+        print 'Usage: python uaclient.py config method option'
+        raise SystemExit
+
+    METODO = str(sys.argv[2])
+    FICHERO = str(sys.argv[1])
+    
+
     parser = make_parser()
     chandler = XMLHandler()
-    print chandler.dic_etiq
     parser.setContentHandler(chandler)
-    parser.parse(open('ua1.xml'))
-    #print chandler.get_tags()
-    chandler.extraer_valores()
+    parser.parse(open(FICHERO))
+    
+    USERNAME = chandler.dic_etiq['account_username']
+    
     print chandler.dic_etiq
+    # Contenido que vamos a enviar
+    LINE = METODO + " sip:" + LOGIN + "@" + IP_SERVER + " SIP/2.0\r\n\r\n"
+    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    my_socket.connect((IP_SERVER, PORT))
+
+    try:
+        #Envio del mensaje
+        my_socket.send(LINE + '\r\n')
+        print "Enviando: " + LINE
+        #Recibimos el mensaje
+        data = my_socket.recv(1024)
+    except socket.error:
+        print 'Error: No server listening at ' + IP_SERVER + ' port ' + str(PORT)
+        raise SystemExit
+
+    print 'Recibido -- \r\n\r\n', data
+
+    if METODO == "INVITE":
+        if data.split("\r\n\r\n")[0] == "SIP/2.0 100 Trying":
+            if data.split("\r\n\r\n")[1] == "SIP/2.0 180 Ringing":
+                if data.split("\r\n\r\n")[2] == "SIP/2.0 200 OK":
+                    ack = "ACK sip:" + LOGIN + "@" + IP_SERVER + " SIP/2.0\r\n\r\n"
+                    my_socket.send(ack + "\r\n")
+
+    print "Terminando socket..."
+    my_socket.close()
+    print "Fin."
