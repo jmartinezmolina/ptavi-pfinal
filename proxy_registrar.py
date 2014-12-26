6c7eb737418cab61bffad7557fc94faf6ad6b27d
nathdiaza <nath.diaza@gmail.com>
2014-12-26 14:22:19 +0100

    Implementación si llega un Invite

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -43,7 +43,6 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
     Echo server class
     """
     def handle(self):
-        ip = self.client_address[0]
         while 1:
             # Leyendo línea a línea lo que nos llega
             line = self.rfile.read()
@@ -51,63 +50,97 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                 break
             print "Recibimos: " + line
             lista = line.split()
-
+            ip = self.client_address[0]
+                        # --->El proxy debe ver si está bien el método y petición o reenvía lo que llegue?
+            # Ver si el método llegado es correcto
             metodo = lista[0]
             metodos_SIP = ("REGISTER", "INVITE", "BYE", "ACK")
 
-            # Si el método es REGISTER
-            if metodo == "REGISTER":
-                # Primero vemos si alguien ha expirado
-                self.ver_si_expire()
-                # Cliente nuevo según su expire
-                user = lista[1].split(":")[1]
-                            #---> ese puerto o del que le llega?
-                puerto = int(lista[1].split(":")[2])
-                expires = int(lista[4])
-                if expires > 0:
-                    if not user in diccionario:
-                        print "... entra en el dicc: " + str(user) + "\r"
-                    else:
-                        print "... modifico del dicc: " + str(user) + "\r"
-                    time_now = time.gmtime(time.time())
-                    day_time_now = time.strftime(formato_time, time_now)
-                    time_exp = time.gmtime(time.time() + expires)
-                    day_time_exp = time.strftime(formato_time, time_exp)
-                    diccionario[user] = [ip, puerto, day_time_now, day_time_exp]
-                    print "diccionario: " + str(diccionario) + "\r"
-                else:
-                    if user in diccionario:
-                        del diccionario[user]
-                        print "... borro del dicc a: " + str(user) + "\r"
-                        if diccionario:
-                            print "diccionario: " + str(diccionario) + "\r"
-                self.register2file()
-                respuesta = "SIP/2.0 200 OK \r\n\r\n"
+            if not metodo in metodos_SIP:
+                self.wfile.write('SIP/2.0 405 Method Not Allowed\r\n\r\n')
+                print 'Respondo: SIP/2.0 405 Method Not Allowed\r\n\r\n'
             
-            # Si el método es INVITE
-            elif metodo == "INVITE":
-                        # ---> mirar si ha expirado?
-                # Primero vemos si alguien ha expirado
-                self.ver_si_expire()
-                # Vemos si está registrado al que se quiere hacer invite
-                name_invite = lista[1].split(":")[1]
-                registrado = 0
-                self.ver_si_registered(name_invitado, registrado)
-                if registrado == 0:
-
-                else:
-                
-
-
-
             else:
-                respuesta = "SIP/2.0 400 Bad Request \r\n\r\n"
+                # Ver si la petición está bien formada
+                protocolo = line.split()[1].split(':')[0]
+                direc = line.split()[1].split(':')[1]
+                sip_v = line.split()[2]
 
-            print "\r\n\r\nRespondo al cliente: " + respuesta
-            self.wfile.write(respuesta)
+                if protocolo == "sip" and "@" in direc and sip_v == "SIP/2.0":
+
+                    # Si el método es REGISTER
+                    if metodo == "REGISTER":
+                        # Primero vemos si alguien ha expirado
+                        self.ver_si_expire()
+                        # Cliente nuevo según su expire
+                        user = lista[1].split(":")[1]
+                                    #---> ese puerto o del que le llega? self.client_address[1] 
+                        puerto = int(lista[1].split(":")[2])
+                        expires = int(lista[4])
+                        if expires > 0:
+                            if not user in diccionario:
+                                print "... entra en el dicc: " + str(user) + "\r"
+                            else:
+                                print "... modifico del dicc: " + str(user) + "\r"
+                            time_now = time.gmtime(time.time())
+                            day_time_now = time.strftime(formato_time, time_now)
+                            time_exp = time.gmtime(time.time() + expires)
+                            day_time_exp = time.strftime(formato_time, time_exp)
+                            diccionario[user] = [ip, puerto, day_time_now, day_time_exp]
+                            print "diccionario: " + str(diccionario) + "\r"
+                        else:
+                            if user in diccionario:
+                                del diccionario[user]
+                                print "... borro del dicc a: " + str(user) + "\r"
+                                if diccionario:
+                                    print "diccionario: " + str(diccionario) + "\r"
+                        self.register2file()
+                        print "Respondo: SIP/2.0 200 OK\r\n\r\n"
+                        self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
+                    
+                    # Si el método es INVITE
+                    elif metodo == "INVITE":
+                        # Primero vemos si alguien ha expirado
+                        self.ver_si_expire()
+                        # Vemos si está registrado al que se quiere hacer invite
+                        name_invite = lista[1].split(":")[1]
+                        registrado = 0
+                        self.ver_si_registered(name_invitado, registrado)
+
+                        # Si está registrado y no ha expirado lo reenvío al que va el invite
+                        if registrado == 1:
+                            my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                            my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                            my_socket.connect((diccionario[name_invite][0], diccionario[name_invite][1]))
+                            print "\nReenviando: " + line
+                            my_socket.send(line + '\r\n')
+                            # Error si el servidor no está lanzado
+                            try:
+                                data = my_socket.recv(1024)
+                            except socket.error:
+                                error = "Error: No server listening at "
+                                error += diccionario[name_invite][0] + " port "
+                                error += str(diccionario[name_invite][1]) + "\r\n"
+                                print error
+                                raise SystemExit
+                            # Recibo respuesta y la envío al que solicitó el invite
+                            print 'Recibido -- ', data
+                            self.wfile.write(data)
+                            print "Respondo: " + data
+
+                        # Si no está registrado o sí lo estaba pero ha expirado
+                        else:
+                            print "Respondo: SIP/2.0 404 User Not Found\r\n\r\n"
+                            self.wfile.write('SIP/2.0 404 User Not Found\r\n\r\n')
+
+                    #elif metodo == "BYE":
 
 
-
+                    #elif metodo == "ACK":
+                    
+                else:
+                    self.wfile.write('SIP/2.0 400 Bad Request\r\n\r\n')
+                    print 'Respondo: SIP/2.0 400 Bad Request\r\n\r\n'
 
 
     """
@@ -145,7 +178,7 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
         for user in diccionario.keys():
             if name_invite == user:
                 registrado = 1
-            else
+            else:
                 registrado = 0            
 
 
@@ -192,6 +225,6 @@ if __name__ == "__main__":
     """
     # Creamos servidor de eco y escuchamos
             # ---> q puerto ponemos?
-    serv = SocketServer.UDPServer((xml["server_ip"]), int(xml["server_puerto"])), EchoHandler)
+    serv = SocketServer.UDPServer((xml["server_ip"], int(xml["server_puerto"])), EchoHandler)
     print "\nServer " + xml["server_name"] + " listening at port " + xml["server_puerto"] + "...\r\n"
     serv.serve_forever()
