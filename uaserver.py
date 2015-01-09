6ebd146936301baae094788844e255274dd47760
Nath <nath@nath-VirtualBox.(none)>
2015-01-09 13:37:36 +0100

    Parte básica, con pep8

--- uaserver.py
+++ uaserver.py
@@ -10,17 +10,12 @@ import time
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 from uaclient import XMLHandler
-
-"""
-CLASE ECHO HANDLER
-"""
-SDP_uac_ip = ""
-SDP_uac_puerto = 0
+from uaclient import Log
 
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
     """
-    Echo server class
+    CLASE ECHO HANDLER
     """
     def handle(self):
         while 1:
@@ -30,14 +25,14 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                 break
             rec_ip = self.client_address[0]
             rec_puerto = self.client_address[1]
-            self.log_rec(rec_ip, rec_puerto, line)
+            Log.log_rec(rec_ip, rec_puerto, line, f_log)
             # Ver si el método llegado es correcto
             metodo = line.split()[0]
             metodos_SIP = ("INVITE", "BYE", "ACK")
 
             if metodo not in metodos_SIP:
                 line_send = 'SIP/2.0 405 Method Not Allowed\r\n'
-                self.log_send(rec_ip, rec_puerto, line_send)
+                Log.log_send(rec_ip, rec_puerto, line_send, f_log)
                 self.wfile.write(line_send + "\r\n")
 
             else:
@@ -62,74 +57,52 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                         try:
                             SDP_uac = line.split('\r\n')[3:]
                             # Busco en el sdp recibido la ip y el puerto
-                            for cab in range(len(SDP_uac)):
-                                if SDP_uac[cab].split("=")[0] == "o":
-                                    ip_rtp = SDP_uac[cab].split("=")[1].split(" ")[1]
-                                elif SDP_uac[cab].split("=")[0] == "m":
-                                    puerto_rtp = SDP_uac[cab].split("=")[1].split(" ")[1]
-                            # Guardo la ip y puerto para poder acceder más tarde
-                            Lista_SDP_uac = [ip_rtp, puerto_rtp]
+                            for linea in range(len(SDP_uac)):
+                                cabecera = SDP_uas[linea].split("=")
+                                if cabecera[0] == "o":
+                                    rtp_ip = cabecera[1].split(" ")[1]
+                                elif cabecera[0] == "m":
+                                    rtp_p = cabecera[1].split(" ")[1]
+                            # Guardo ip y puerto para acceder más tarde
+                            Lista_SDP_uac = [rtp_ip, rtp_p]
                             Dicc_SDP_uac['key'] = Lista_SDP_uac
                             # Respondo al Invite
                             line_send = 'SIP/2.0 100 Trying\r\n'
-                            self.log_send(rec_ip, rec_puerto, line_send)
+                            Log.log_send(rec_ip, rec_puerto, line_send, f_log)
                             self.wfile.write(line_send + "\r\n")
                             line_send = 'SIP/2.0 180 Ringing\r\n'
-                            self.log_send(rec_ip, rec_puerto, line_send)
+                            Log.log_send(rec_ip, rec_puerto, line_send, f_log)
                             self.wfile.write(line_send + "\r\n")
                             line_send = 'SIP/2.0 200 OK\r\n' + SDP_uas
-                            self.log_send(rec_ip, rec_puerto, line_send)
+                            Log.log_send(rec_ip, rec_puerto, line_send, f_log)
                             self.wfile.write(line_send + "\r\n")
                         except IndexError:
                             line_send = 'SIP/2.0 400 Bad Request\r\n'
-                            self.log_send(rec_ip, rec_puerto, line_send)
+                            Log.log_send(rec_ip, rec_puerto, line_send, f_log)
                             self.wfile.write(line_send + "\r\n")
 
                     elif metodo == 'ACK':
-                                # --> no funcionaaaaaaaaaaaa
-                        # ENVIO RTP
-                        ip = Dicc_SDP_uac['key'][0]
-                        puerto = str(Dicc_SDP_uac['key'][1])
+                        # Envio RTP
+                        rtp_ip = Dicc_SDP_uac['key'][0]
+                        rtp_p = str(Dicc_SDP_uac['key'][1])
                         # run: lo que se ha de ejecutar en la shell
-                        run = './mp32rtp -i ' + ip + " -p " + puerto
+                        run = './mp32rtp -i ' + rtp_ip + " -p " + rtp_p
                         run += " < " + xml["audio_path"]
                         print "Vamos a ejecutar", run
                         os.system(run)
                         line_send = "RTP audio\r\n"
-                        self.log_send(ip, puerto, line_send)
+                        Log.log_send(rtp_ip, rtp_p, line_send, f_log)
                         print "\r\nEl fichero de audio ha finalizado\r\n\r\n"
 
                     elif metodo == 'BYE':
                         line_send = 'SIP/2.0 200 OK\r\n'
-                        self.log_send(rec_ip, rec_puerto, line_send)
+                        Log.log_send(rec_ip, rec_puerto, line_send, f_log)
                         self.wfile.write(line_send + "\r\n")
 
                 else:
                     line_send = 'SIP/2.0 400 Bad Request\r\n'
-                    self.log_send(rec_ip, rec_puerto, line_send)
+                    Log.log_send(rec_ip, rec_puerto, line_send, f_log)
                     self.wfile.write(line_send + "\r\n")
-
-    """
-    Guardar mensajes de depuración de envío en .log
-    """
-    def log_send(self, ip, puerto, line_send):
-        log = open(xml["log_path"], 'a')
-        send_to = "Sent to " + ip + ":" + str(puerto)
-        print "\n" + send_to
-        line_log = send_to + ": " + line_send.replace('\r\n', ' ') + '\r\n'
-        log.write(time.strftime('%Y%m%d%H%M%S') + ' ' + line_log)
-        print '\nEnviando: ' + line_send
-
-    """
-    Guardar mensajes de depuración de recibo en .log
-    """
-    def log_rec(self, ip, puerto, line_rec):
-        log = open(xml["log_path"], 'a')
-        rec_from = 'Received from ' + ip + ":" + str(puerto)
-        print "\n" + rec_from
-        line_log = rec_from + ": " + line_rec.replace('\r\n', ' ') + '\r\n'
-        log.write(time.strftime('%Y%m%d%H%M%S') + ' ' + line_log)
-        print "\nRecibido -- " + line_rec
 
 
 if __name__ == "__main__":
@@ -171,6 +144,10 @@ if __name__ == "__main__":
     """
     """
     Dicc_SDP_uac = {}
+    # log, Herencia del cliente
+    Log = Log()
+    f_log = xml["log_path"]
+    form_log = time.strftime('%Y%m%d%H%M%S')
 
     """
     SOCKET
@@ -182,16 +159,16 @@ if __name__ == "__main__":
     try:
         print '\nListening...'
         # log
-        log = open(xml["log_path"], 'a')
-        log.write(time.strftime('%Y%m%d%H%M%S') + ' ' + 'Starting...\r\n')
+        log = open(f_log, 'a')
+        log.write(form_log + ' ' + 'Starting...\r\n')
         log.close()
         # Creamos servidor de eco y escuchamos
         serv = SocketServer.UDPServer((server_ip, server_p), EchoHandler)
         serv.serve_forever()
     except(KeyboardInterrupt):
         # log
-        log = open(xml["log_path"], 'a')
-        log.write(time.strftime('%Y%m%d%H%M%S') + ' ' + '...Finishing.\r\n\r\n')
+        log = open(f_log, 'a')
+        log.write(form_log + ' ' + '...Finishing.\r\n\r\n')
         log.close()
         print "\nTerminando server..."
         print "\nFin.\r\n"
